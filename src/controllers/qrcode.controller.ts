aa2394e57fe82dad648bf18f650d5237bf173b6b
developer <85758481+narzullayev0772@users.noreply.github.com>
2024-02-06 19:00:23 +0500

    Update qrcode.controller.ts

--- src/controllers/qrcode.controller.ts
+++ src/controllers/qrcode.controller.ts
@@ -1,79 +1,83 @@
-import {Request, Response} from "express";
-import {PassThrough} from "stream";
+import { Request, Response } from "express";
+import { PassThrough } from "stream";
 import QRCode from "qrcode";
-import {v4} from "uuid";
-import {DocumentSchema} from "../database/document.scema";
+import { v4 } from "uuid";
+import { DocumentSchema } from "../database/document.scema";
 
 abstract class AbstractQrCodeController {
-    abstract generateQrCode(req: Request, res: Response): Promise<void>;
+  abstract generateQrCode(req: Request, res: Response): Promise<void>;
 
-    abstract scanQrCode(req: Request, res: Response): Promise<void>;
+  abstract scanQrCode(req: Request, res: Response): Promise<void>;
 }
 
 class QrCodeController extends AbstractQrCodeController {
-    async generateQrCode(req: Request, res: Response): Promise<void> {
-        try {
-            const width = req.query.width ? parseFloat(req.query.width.toString()) : 200;
-            const qrStream = new PassThrough();
-            const uuid = v4();
-            const scanUrl = `${process.env.QR_CODE_SCAN_URL}/${uuid}`;
-            await QRCode.toFileStream(qrStream, scanUrl,
-                {
-                    type: 'png',
-                    width: width,
-                    errorCorrectionLevel: 'H'
-                }
-            );
-            res.setHeader('x-qrcode-id', uuid);
-            res.setHeader('Content-Type', 'image/png');
-            res.setHeader('Content-Disposition', 'attachment; filename=qr-code.png');
-            qrStream.pipe(res);
-        } catch (err) {
-            console.error('Failed to return content', err);
-        }
+  async generateQrCode(req: Request, res: Response): Promise<void> {
+    try {
+      const width = req.query.width
+        ? parseFloat(req.query.width.toString())
+        : 200;
+      const qrStream = new PassThrough();
+      const uuid = v4();
+      const scanUrl = `${process.env.QR_CODE_SCAN_URL}/${uuid}`;
+      await QRCode.toFileStream(qrStream, scanUrl, {
+        type: "png",
+        width: width,
+        errorCorrectionLevel: "H",
+      });
+      res.setHeader("x-qrcode-id", uuid);
+      res.setHeader("Content-Type", "image/png");
+      res.setHeader("Content-Disposition", "attachment; filename=qr-code.png");
+      qrStream.pipe(res);
+    } catch (err) {
+      console.error("Failed to return content", err);
     }
+  }
 
-    async scanQrCode(req: Request, res: Response): Promise<void> {
-        try {
-            const documentId = req.params.id;
-            const document = await DocumentSchema.findById(documentId, {}, {
-                populate: [
-                    {
-                        path: 'by',
-                        select: 'name file',
-                        populate: {
-                            path: 'file',
-                            select: '_id'
-                        }
-                    }
-                ]
-            });
-            if (!document) {
-                res.send('Document not found');
-            } else {
-                const doc = {
-                    number: document.number,
-                    customerName: document.customerName,
-                    value: document.value,
-                    date: document.date.toLocaleDateString(
-                        'uz-UZ',
-                        {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric'
-                        }
-                    ),
-                    scannedFile: `${process.env.QR_CODE_SCAN_URL}/download/${document.file}`,
-                    byFile: `${process.env.QR_CODE_SCAN_URL}/download/${document.by.file._id}`,
-                    byName: document.by.name
-                };
-                res.render('scan', doc);
-            }
-        } catch (err) {
-            res.send('Document not found');
+  async scanQrCode(req: Request, res: Response): Promise<void> {
+    try {
+      const fileId = req.params.id;
+      const document = await DocumentSchema.findOne(
+        {
+          file: {
+            name: `${fileId}.pdf`,
+          },
+        },
+        {},
+        {
+          populate: [
+            {
+              path: "by",
+              select: "name file",
+              populate: {
+                path: "file",
+                select: "_id",
+              },
+            },
+          ],
         }
+      );
+      if (!document) {
+        res.send("Document not found");
+      } else {
+        const doc = {
+          number: document.number,
+          customerName: document.customerName,
+          value: document.value,
+          date: document.date.toLocaleDateString("uz-UZ", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+          scannedFile: `${process.env.QR_CODE_SCAN_URL}/download/${document.file}`,
+          byFile: `${process.env.QR_CODE_SCAN_URL}/download/${document.by.file._id}`,
+          byName: document.by.name,
+        };
+        res.render("scan", doc);
+      }
+    } catch (err) {
+      res.send("Document not found");
     }
+  }
 }
-
 
 export default new QrCodeController();
